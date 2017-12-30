60d1079493c4cc0f78cf932913557bc392a90616
shinfkd <2392320+shinfkd@users.noreply.github.com>
2017-12-30 19:05:35 +0900

    ディレクトリ構成変更

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const GarbageNotifier = require('./GarbageNotifier');
+const GarbageNotifier = require('./src/GarbageNotifier');
 const notifier = new GarbageNotifier();
 const weekday = notifier.getCurrentWeekday();
 notifier.notify(weekday);
