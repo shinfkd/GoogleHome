beb93e38eb3acd82952bf748eebe55d81dea1441
shinfkd <2392320+shinfkd@users.noreply.github.com>
2018-01-08 19:04:48 +0900

    TargetDate#weekdayOrdinalのテスト追加

--- test/TargetDate.spec.js
+++ test/TargetDate.spec.js
@@ -17,4 +17,31 @@ describe('TargetDate', () => {
       assert.equal(targetDate.weekday(), 7);
     });
   });
+
+  describe('#weekdayOrdinal()', () => {
+    it('1日は第1曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-01'));
+      assert.equal(targetDate.weekdayOrdinal(), 1);
+    });
+    it('7日は第1曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-08'));
+      assert.equal(targetDate.weekdayOrdinal(), 2);
+    });
+    it('8日は第2曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-08'));
+      assert.equal(targetDate.weekdayOrdinal(), 2);
+    });
+    it('14日は第2曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-14'));
+      assert.equal(targetDate.weekdayOrdinal(), 2);
+    });
+    it('29日は第5曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-29'));
+      assert.equal(targetDate.weekdayOrdinal(), 5);
+    });
+    it('31日は第5曜日になる', () => {
+      targetDate = new TargetDate(moment('2018-01-31'));
+      assert.equal(targetDate.weekdayOrdinal(), 5);
+    });
+  });
 });
